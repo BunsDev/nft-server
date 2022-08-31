ac472a5e86d519549e5191bafd20daeea2b358bb
patientsmoke <admin@patientsmoke.com>
2022-08-30 16:56:05 -0800

    - cleanup unused crons

--- src/api/crons/index.ts
+++ src/api/crons/index.ts
@@ -1,29 +1,11 @@
 import collectionDiscovery from "./collectionDiscovery";
-import setRecordState from "./setRecordState";
-import updateStatistics from "./updateStatistics";
-import fixSalePK from "./fixSalePK";
-import fixMismatchedSales from "./fixMismatchedSales";
-import findMismatchedSales from "./findMismatchedSales";
-import fixSaleSK from "./fixSaleSK";
 import calculateStatistics from "./calculateStatistics";
-import zeroCollectionStats from "./zeroCollectionStats";
-import createAllCollections from "./createAllCollections";
 import globalStatistics from "./globalStatistics";
-import lateProvider from "./lateProvider";
 import collectionStatistics from "./collectionStatistics";
 
 export default {
   collectionDiscovery,
-  setRecordState,
-  updateStatistics,
-  fixSalePK,
-  fixMismatchedSales,
-  findMismatchedSales,
-  fixSaleSK,
   calculateStatistics,
   globalStatistics,
-  zeroCollectionStats,
-  createAllCollections,
-  lateProvider,
   collectionStatistics,
 };

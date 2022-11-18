0cecff1acf56c2db40f12a997883ed085c0d6daa
waynebruce0x <waynebruce0x@gmail.com>
2022-11-18 13:21:29 +0000

    metas successful

--- src/markets/x2y2/x2y2.ts
+++ src/markets/x2y2/x2y2.ts
@@ -18,10 +18,57 @@ import { ClusterWorker, IClusterProvider } from "../../utils/cluster";
 import dynamodb from "../../utils/dynamodb";
 import BaseProvider from "../BaseProvider";
 
+const GET_BLOCK_PARALLELISM: number = process.env.GET_BLOCK_PARALLELISM
+  ? parseInt(process.env.GET_BLOCK_PARALLELISM)
+  : 5;
+const MATURE_BLOCK_AGE = process.env.MATURE_BLOCK_AGE
+  ? parseInt(process.env.MATURE_BLOCK_AGE)
+  : 250;
+const BLOCK_RANGE = 50;
+
 // const LOGGER = getLogger("X2Y2_PROVIDER", {
 //   datadog: !!process.env.DATADOG_API_KEY
 // });
+export type MatchData = {
+  transactionHash: string;
+  buyer: string;
+  contractAddress: string;
+  tokenIDs: string[];
+  quantity: number;
+};
+function addTradeToDatas(
+  transfer: Log,
+  payment: Payment,
+  datas: MatchData[],
+  quantity: number
+): void {
+  let newEntry: MatchData = undefined;
+  if (transfer.topics[0] == consts.transferTopic)
+    newEntry = {
+      transactionHash: transfer.transactionHash,
+      buyer: `0x${transfer.topics[2].substring(26, 66)}`,
+      contractAddress: transfer.address,
+      tokenIDs: [parseInt(transfer.topics[3], 16).toString()],
+      quantity
+    };
+  if (transfer.topics[0] == consts.transferSingleTopic)
+    newEntry = {
+      transactionHash: transfer.transactionHash,
+      buyer: `0x${transfer.topics[3].substring(26, 66)}`,
+      contractAddress: transfer.address,
+      tokenIDs: [parseInt(transfer.data.substring(2, 66), 16).toString()],
+      quantity
+    };
 
+  const swapsInThisBundle = datas.find(
+    (d) => d.transactionHash == newEntry.transactionHash
+  );
+  if (swapsInThisBundle == null) {
+    datas.push(newEntry);
+  } else if (!swapsInThisBundle.tokenIDs.includes(newEntry.tokenIDs[0])) {
+    swapsInThisBundle.tokenIDs.push(...newEntry.tokenIDs);
+  }
+}
 type TokenEventMetadataMap = Record<string, EventMetadata>;
 
 export default class X2y2Provider
@@ -45,26 +92,266 @@ export default class X2y2Provider
     return Promise.reject(new Error("Not implemented"));
   }
 
-  public parseEvents(
+  private async fetchMatchData(
+    chain: Blockchain,
+    events: Array<Event>
+  ): Promise<MatchData[]> {
+    const datas: MatchData[] = [];
+    const provider = this.chains[chain].provider;
+    const topics = await Promise.all(
+      events.map((e: Event) =>
+        provider.getTransactionReceipt(e.transactionHash)
+      )
+    );
+    const transactions = await Promise.all(
+      events.map((e: Event) => provider.getTransaction(e.transactionHash))
+    );
+    topics.map((topic: any, i: number) => {
+      let payment: Payment = unknownPayment;
+      let transfers: Log[] = [
+        ...topic.logs.filter(
+          (log: Log) =>
+            consts.transferTopic == log.topics[0] &&
+            log.topics[1] != consts.nullAddress
+        ),
+        ...topic.logs.filter(
+          (log: Log) =>
+            consts.transferSingleTopic == log.topics[0] &&
+            log.topics[2] != consts.nullAddress
+        )
+      ];
+
+      const matches: Log[] = topic.logs.filter((log: Log) =>
+        [consts.matchTopic].includes(log.topics[0])
+      );
+
+      if (matches.length != transfers.length && transfers.length > 0) {
+        console.log("bang");
+      }
+      transfers.map((transfer: Log) => {
+        if (!transactions[i].value.eq(BigNumber.from(0))) {
+          payment = {
+            address: consts.gasToken,
+            amount: transactions[i].value
+          };
+        } else if (payment.address == "0x") {
+          console.error("PAYMENT HAS NOT RESOLVED");
+          return;
+        }
+        addTradeToDatas(transfer, payment, datas, matches.length);
+      });
+    });
+
+    return datas;
+  }
+
+  public async *fetchSales(): AsyncGenerator<ChainEvents> {
+    // eslint-disable-next-line no-unreachable-loop
+    for (const chain of Object.keys(this.chains) as Blockchain[]) {
+      const { deployBlock, contractAddress, providerName, adapterRunName } =
+        this.config.chains[chain];
+      const currentBlock: number = await this.chains[
+        chain
+      ].getCurrentBlockNumber();
+      const lastMatureBlock = currentBlock - MATURE_BLOCK_AGE;
+      let { lastSyncedBlockNumber } = await AdapterState.getSalesAdapterState(
+        this.market,
+        chain,
+        true,
+        deployBlock,
+        adapterRunName ?? providerName
+      );
+      if (deployBlock && Number.isInteger(deployBlock)) {
+        if (lastSyncedBlockNumber < deployBlock) {
+          // AdapterState.updateSalesLastSyncedBlockNumber(
+          //   this.market,
+          //   deployBlock,
+          //   chain,
+          //   adapterRunName ?? providerName
+          // );
+        }
+        lastSyncedBlockNumber = Math.max(deployBlock, lastSyncedBlockNumber);
+      }
+      const contract = this.contracts[chain];
+      const filterTopics = this.config.chains[chain].saleTopic
+        ? [this.config.chains[chain].saleTopic]
+        : this.contracts[chain].interface.encodeFilterTopics(
+            this.contracts[chain].interface.getEvent(
+              this.config.chains[chain].saleEventName
+            ),
+            []
+          );
+
+      if (lastMatureBlock - lastSyncedBlockNumber <= MATURE_BLOCK_AGE) {
+        // LOGGER.error(`Not enough mature blocks to scan.`, {
+        //   currentBlock,
+        //   lastMatureBlock,
+        //   lastSyncedBlockNumber
+        // });
+        return;
+      }
+
+      let retryCount = 0;
+      let retryQuery = false;
+
+      for (
+        let i = 0;
+        i < lastMatureBlock - lastSyncedBlockNumber;
+        i += BLOCK_RANGE + 1
+      ) {
+        const fromBlock = lastSyncedBlockNumber + i;
+        const toBlock =
+          fromBlock + BLOCK_RANGE > currentBlock
+            ? currentBlock
+            : fromBlock + BLOCK_RANGE;
+
+        // LOGGER.debug("Searching blocks: ", {
+        //   fromBlock,
+        //   toBlock,
+        //   range: toBlock - fromBlock,
+        // });
+
+        // if (retryQuery) {
+        //   LOGGER.warn(`Retrying query`, {
+        //     fromBlock,
+        //     toBlock,
+        //     range: toBlock - fromBlock,
+        //     retryCount,
+        //   });
+        // }
+
+        try {
+          const queryFilterStart = performance.now();
+          const events: Array<Event> = (
+            await contract.queryFilter(
+              {
+                address: contractAddress,
+                topics: filterTopics
+              },
+              fromBlock,
+              toBlock
+            )
+          ).filter((e) => !e.removed);
+          const queryFilterEnd = performance.now();
+          // this.MetricsReporter.submit(
+          //   `opensea_seaport.${chain}.contract_queryFilter.blockRange`,
+          //   toBlock - fromBlock
+          // );
+          // this.MetricsReporter.submit(
+          //   `opensea_seaport.${chain}.contract_queryFilter.latency`,
+          //   queryFilterEnd - queryFilterStart
+          // );
+
+          // LOGGER.debug(
+          //   `Found ${events.length} events between ${fromBlock} to ${toBlock}`
+          // );
+
+          // LOGGER.debug("Seaport Events", { fromBlock, toBlock, events });
+
+          if (events.length) {
+            this.retrieveBlocks(fromBlock, toBlock, chain);
+            const blocks = (
+              await Promise.all(this.getBlockList(fromBlock, toBlock))
+            ).reduce(
+              (m: Record<string, Block>, b: Block) => ({
+                ...m,
+                [b.number.toString()]: b
+              }),
+              {} as Record<string, Block>
+            );
+
+            const receipts: TxReceiptsWithMetadata = {};
+            const matchData: MatchData[] = await this.fetchMatchData(
+              chain,
+              events
+            );
+            const parsedEvents = this.parseEventsWithMatchData(
+              events,
+              chain,
+              matchData
+            );
+            for (let i = 0; i < events.length; i++) {
+              const event = events[i];
+              const parsed = parsedEvents[i];
+              if (!(event.transactionHash in receipts)) {
+                receipts[event.transactionHash] = {
+                  receipt: {
+                    blockNumber: event.blockNumber,
+                    transactionHash: event.transactionHash
+                  } as ReceiptLike,
+                  meta: [] as Array<EventMetadata>
+                };
+              }
+              receipts[event.transactionHash].meta.push(parsed);
+            }
+
+            yield {
+              blocks,
+              chain,
+              events,
+              blockRange: {
+                startBlock: fromBlock,
+                endBlock: toBlock
+              },
+              receipts,
+              providerName,
+              adapterRunName
+            };
+          } else {
+            yield {
+              chain,
+              events,
+              blockRange: {
+                startBlock: fromBlock,
+                endBlock: toBlock
+              },
+              providerName,
+              adapterRunName
+            };
+          }
+
+          retryCount = 0;
+          retryQuery = false;
+        } catch (e) {
+          // LOGGER.error(`Query error`, {
+          //   error: /quorum/.test(e.message) ? `Quorum error` : e.message,
+          //   reason: e.reason,
+          //   fromBlock,
+          //   toBlock,
+          //   stack: e.stack.substr(0, 500)
+          // });
+          if (retryCount < 3) {
+            // try again
+            retryCount++;
+            i -= i - (BLOCK_RANGE + 1) < 0 ? i : BLOCK_RANGE + 1;
+            retryQuery = true;
+          } else if (retryCount > 3) {
+            // LOGGER.error(`Not able to recover from query errors`);
+            throw new Error(`Not able to recover from query errors`);
+          }
+        }
+      }
+    }
+  }
+
+  public parseEventsWithMatchData(
     events: Array<Event>,
-    chain: Blockchain
+    chain: Blockchain,
+    matchDatas: MatchData[]
   ): Array<EventMetadata> {
     const { providerName } = this.config.chains[chain];
     const meta: Array<EventMetadata> = [];
-    const transferTopics: Transfer[] = [];
     events = events.filter(
       (e) =>
         e.transactionHash ==
         "0x4684edf0c9a7b615a15bf8d57f10f3937cf7f6f699cbcb53f201fcaa955bcf17"
     );
-    fetchTransferTopics(chain, events).then((r) => transferTopics.push(r));
     for (const event of events) {
       const parsed = this.parseLog(event, chain);
       const { currency, amount, to } = parsed.decodedData;
-      const transferTopic = transferTopics.find(
+      const matchData = matchDatas.find(
         (t) => (t.transactionHash = event.transactionHash)
       );
-
       // LOGGER.debug(`X2y2 Event`, {
       //   saleEvent,
       //   buyer,
@@ -78,28 +365,28 @@ export default class X2y2Provider
       //   event
       // });
 
-      // buyer, contract, price, tokenId, count, bundle,
       meta.push({
-        buyer: transferTopic.buyer, // 0x33a34e27a81436ba9d79276406a285e89a8bd8a8
+        buyer: matchData.buyer,
         seller: to,
-        contractAddress: transferTopic.contractAddress, //collection, // 0xDA4c9FFB9a96ef44865114Be4af25004f0eE385d
+        contractAddress: matchData.contractAddress,
         eventSignatures: [event.eventSignature],
         payment: {
           address: currency,
           amount
         },
         price: amount,
-        tokenID: transferTopic.tokenId, //tokenId.toString(), // 6275
-        count: transferTopic.count, // 1
+        tokenID: matchData.quantity > 1 ? null : matchData.tokenIDs[0],
+        count: matchData.quantity, // 1
         data: {
           parsed,
-          event
+          event,
+          tokenIDs: matchData.quantity > 1 ? matchData.tokenIDs : null
         },
         hash: event.transactionHash,
         contract: providerName,
         logIndex: event.logIndex,
         blockNumber: event.blockNumber,
-        bundleSale: false
+        bundleSale: matchData.quantity > 1
       });
     }
     return meta;
@@ -128,47 +415,10 @@ type Log = {
 const unknownPayment: Payment = { address: "0x", amount: BigNumber.from(0) };
 const consts: any = {
   transferTopic:
-    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-};
-async function fetchTransferTopics(
-  chain: Blockchain,
-  events: Array<Event>
-): Promise<Transfer> {
-  const provider = this.chains[chain].provider;
-  const topics = await Promise.all(
-    events.map((e: Event) => provider.getTransactionReceipt(e.transactionHash))
-  );
-  const transactions = await Promise.all(
-    events.map((e: Event) => provider.getTransaction(e.transactionHash))
-  );
-  topics.map((topic: any, i: number) => {
-    let payment: Payment = unknownPayment;
-    let transfers: Log[] = [
-      ...topic.logs.filter(
-        (log: Log) =>
-          consts.transferTopic == log.topics[0] &&
-          log.topics[1] != consts.nullAddress
-      ),
-      ...topic.logs.filter(
-        (log: Log) =>
-          consts.transferSingleTopic == log.topics[0] &&
-          log.topics[2] != consts.nullAddress
-      )
-    ];
-
-    const matches: Log[] = topic.logs.filter((log: Log) =>
-      [consts.matchTopic].includes(log.topics[0])
-    );
-
-    if (matches.length != transfers.length && transfers.length > 0) {
-      console.log("bang");
-    }
-  });
-  return {
-    transactionHash: "",
-    buyer: "",
-    contractAddress: "",
-    tokenId: "",
-    count: 1
-  };
-}
+    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+  matchTopic:
+    "0xe2c49856b032c255ae7e325d18109bc4e22a2804e2e49a017ec0f59f19cd447b",
+  gasToken: "0x0000000000000000000000000000000000000000",
+  nullAddress:
+    "0x0000000000000000000000000000000000000000000000000000000000000000"
+}; // ts-node src/adapters/x2y2.ts
